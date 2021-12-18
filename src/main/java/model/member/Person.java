1448e66909db790f1836d2750bbaaaf404760238
negin <negin.s.mousavi@gmail.com>
2021-12-18 22:20:36 +0330

    packaging

---
+++ src/main/java/model/member/Person.java
@@ -0,0 +1,19 @@
+package model.member;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author Negin Mousavi
+ */
+@Data
+@MappedSuperclass
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected int id;
+    @Column(unique = true)
+    protected String username;
+    protected String password;
+}
---
+++ src/main/java/model/member/User.java
@@ -0,0 +1,17 @@
+package model.member;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Negin Mousavi
+ */
+@ToString(callSuper = true)
+@Setter
+@Getter
+@Entity
+public class User extends Person {
+}
