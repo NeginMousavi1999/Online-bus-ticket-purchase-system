7aa631e46965388b6839312c4137555a02f59494
negin <negin.s.mousavi@gmail.com>
2021-12-19 13:26:40 +0330

    add bus dao

--- src/main/java/dao/BusDao.java
+++ src/main/java/dao/BusDao.java
@@ -1,4 +1,35 @@
-package dao;/**
+package dao;
+
+import enumuration.BusType;
+import model.Bus;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+/**
  * @author Negin Mousavi
- */public class BusDao {
+ */
+public class BusDao extends BaseDao {
+
+    public void save(Bus bus) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(bus);
+        transaction.commit();
+        session.close();
+    }
+
+    public List<Bus> get(BusType busType) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        String hql = "FROM Bus b where b.type=:busType";
+        Query<Bus> query = session.createQuery(hql, Bus.class);
+        query.setParameter("busType", busType);
+        List<Bus> list = query.list();
+        transaction.commit();
+        session.close();
+        return list;
+    }
 }
