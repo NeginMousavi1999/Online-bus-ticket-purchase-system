3844a422c24d56bced92c197707299549ee04428
negin <negin.s.mousavi@gmail.com>
2021-12-19 12:21:26 +0330

    add test buy method for ticket

---
+++ src/test/java/model/TicketBuyTest.java
@@ -0,0 +1,34 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author Negin Mousavi
+ */
+public class TicketBuyTest {
+    Ticket ticket = new Ticket();
+    Bus bus = new Bus();
+
+    @BeforeEach
+    void init() {
+        bus.setTotalSeats(10);
+        bus.setSeatsRemaining(8);
+        ticket.setBus(bus);
+    }
+
+    @Test
+    void givenCountOfTicketLessThanRemaining_WhenBuyCalls_ThenResponseReturn() {
+        ticket.buy(5);
+        assertEquals(ticket.getBus().getSeatsRemaining(), 3);
+    }
+
+    @Test
+    void givenCountOfTicketMoreThanRemaining_WhenBuyCalls_ThenExceptionResponseReturn() {
+        Exception exception = assertThrows(RuntimeException.class, () -> ticket.buy(10));
+        assertEquals("not enough tickets!", exception.getMessage());
+    }
+}
