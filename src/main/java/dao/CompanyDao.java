f915848719778c94a28fac4bad0ab84a60cc16a3
negin <negin.s.mousavi@gmail.com>
2021-12-19 13:27:00 +0330

    add company dao

--- src/main/java/dao/CompanyDao.java
+++ src/main/java/dao/CompanyDao.java
@@ -1,4 +1,18 @@
-package dao;/**
+package dao;
+
+import model.Company;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
  * @author Negin Mousavi
- */public class CompanyDao {
+ */
+public class CompanyDao extends BaseDao {
+    public void save(Company company) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(company);
+        transaction.commit();
+        session.close();
+    }
 }
