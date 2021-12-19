6a50749c38afc33bb55aa5eb1c12cd2d0f13f134
negin <negin.s.mousavi@gmail.com>
2021-12-19 14:29:41 +0330

    set abbr

--- src/main/java/enumuration/BusType.java
+++ src/main/java/enumuration/BusType.java
@@ -4,6 +4,16 @@ package enumuration;
  * @author Negin Mousavi
  */
 public enum BusType {
-    VIP,
-    REGULAR;
+    VIP(5.0),
+    REGULAR(2.0);
+
+    double abbr;
+
+    BusType(double abbr) {
+        this.abbr = abbr;
+    }
+
+    public static double getAbbrByValue(BusType value) {
+        return value.abbr;
+    }
 }
