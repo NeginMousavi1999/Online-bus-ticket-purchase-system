d9b7778de6c227f9f91eb00c9fdb3e2b9b55fd58
negin <negin.s.mousavi@gmail.com>
2021-12-19 13:24:46 +0330

    add company service

--- src/main/java/service/CompanyService.java
+++ src/main/java/service/CompanyService.java
@@ -1,4 +1,15 @@
-package service;/**
+package service;
+
+import dao.CompanyDao;
+import model.Company;
+
+/**
  * @author Negin Mousavi
- */public class CompanyService {
+ */
+public class CompanyService {
+    CompanyDao companyDao = new CompanyDao();
+
+    public void save(Company company) {
+        companyDao.save(company);
+    }
 }
