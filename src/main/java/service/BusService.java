27badcc226164e682768720c019ca448fb6a1259
negin <negin.s.mousavi@gmail.com>
2021-12-19 13:24:27 +0330

    add bus service

--- src/main/java/service/BusService.java
+++ src/main/java/service/BusService.java
@@ -1,4 +1,30 @@
-package service;/**
+package service;
+
+import dao.BusDao;
+import enumuration.BusType;
+import model.Bus;
+
+import java.util.List;
+import java.util.Random;
+
+/**
  * @author Negin Mousavi
- */public class BusService {
+ */
+public class BusService {
+    BusDao busDao = new BusDao();
+
+    public Bus getRandomByType(BusType busType) {
+        List<Bus> buses = busDao.get(busType);
+        int randomIndex = generateRandomInt(buses.size());
+        return buses.get(randomIndex);
+    }
+
+    public int generateRandomInt(int bound) {
+        Random rnd = new Random();
+        return rnd.nextInt(bound);
+    }
+
+    public void save(Bus bus) {
+        busDao.save(bus);
+    }
 }
